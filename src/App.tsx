e18055f702b53ee0189362157e08350c9aa37df6
rodolfo.amorim <rodolfo.amorim@RODOLFOAMORIM>
2023-07-05 18:27:54 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import './global.css'
+
+export function App() {
+  return (
+    <main>
+      <nav>
+        <div>
+          <ul>
+            <li>Profile</li>
+            <li>Favorites</li>
+            <li>
+              Balance <span>$ 2.300</span>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <strong>Categoy</strong>
+          <ul>
+            <li>Computer Mouse</li>
+            <li>Game Headphones</li>
+            <li>Gamepads</li>
+            <li>VR Glasses</li>
+            <li>Keybords</li>
+            <li>Computers</li>
+            <li>Games</li>
+          </ul>
+        </div>
+
+        <div>
+          <span>facebook</span>
+          <span>instagram</span>
+          <span>twitter</span>
+        </div>
+
+        <div>
+          <strong>Categoy</strong>
+          <ul>
+            <li>Help</li>
+            <li>Conditions</li>
+            <li>Confidentiality</li>
+          </ul>
+        </div>
+      </nav>
+
+      <div>
+        <h1>Main Content</h1>
+      </div>
+    </main>
+  )
+}
