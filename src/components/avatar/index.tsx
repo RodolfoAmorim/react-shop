58f396264e08c881e5a06be1d834d277e859ae13
Rodolfo Amorim <rodolfoamorim36@gmail.com>
2023-07-24 22:20:50 -0300

    :bug: fix: images urls

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -2,7 +2,7 @@ export function Avatar() {
   return (
     <>
       <img
-        src='/images/users/avatar.png'
+        src='/react-shop/images/users/avatar.png'
         className='w-12 rounded-full outline outline-2 outline-offset-2 outline-sky-300/20 xl:w-10'
       />
       <p className='text-lg leading-relaxed tracking-wider font-normal text-sky-50/80 xl:text-base xl:font-light xl:tracking-wider'>
--- src/components/banners/index.tsx
+++ src/components/banners/index.tsx
@@ -2,7 +2,7 @@ export function Banners() {
   return (
     <section className='w-full px-6 pt-20 xl:p-0'>
       <img
-        src='/images/banners/headphones_banner.png'
+        src='/react-shop/images/banners/headphones_banner.png'
         className='drop-shadow-xl'
       />
     </section>
